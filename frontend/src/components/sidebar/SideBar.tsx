cb561ab15ede74c4e7436d89c73b918edb4043a4
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-14 23:38:31 +0500

    update sidebar

--- frontend/src/components/sidebar/SideBar.tsx
+++ frontend/src/components/sidebar/SideBar.tsx
@@ -2,7 +2,9 @@ import Converstaions from "./conversations/Converstaions"
 import Logout from "./logout/Logout";
 import SearchInput from "./search/SearchInput"
 import { useState } from "react";
-import { RiMenu2Fill } from "react-icons/ri";
+import { BiMenuAltLeft } from "react-icons/bi";
+
+
 
 
 
@@ -14,8 +16,8 @@ const SideBar = () => {
     return (
         <div className={`flex flex-col border-r h-full bg-[#536dfe] relative rounded-l-md ${closeSidebar ? "w-32 smoothanimate" : "w-[400px]"} smoothanimate`}>
 
-            <div className="flex items-center gap-6 h-[70px] border p-6 border-black/20">
-                <RiMenu2Fill onClick={() => setCloseSidebar(!closeSidebar)} />
+            <div className="flex items-center gap-4 h-[70px] border p-6 border-black/20">
+                <BiMenuAltLeft size={24} cursor="pointer" onClick={() => setCloseSidebar(!closeSidebar)} />
                 <SearchInput />
             </div>
             <div className="p-4"></div>
--- frontend/src/components/sidebar/search/SearchInput.tsx
+++ frontend/src/components/sidebar/search/SearchInput.tsx
@@ -1,13 +1,21 @@
-import { IoSearchSharp } from "react-icons/io5";
+import { CgSearch } from "react-icons/cg";
+
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 
 const SearchInput = () => {
-  return (
 
-    <form className='flex items-center gap-2 border-b border-white text-white'>
-      <IoSearchSharp />
-      <input type="text" placeholder='Search...' className='bg-transparent border-none outline-none placeholder:text-white' />
-    </form>
+cost
+
+  return (
+    <div className="flex items-baseline justify-between w-full">
+
+      <CgSearch size={20} cursor="pointer"/>
+      {/* <form className='flex items-center gap-2 border-b border-white text-white'>
+        <input type="text" placeholder='Search...' className='bg-transparent border-none outline-none placeholder:text-white' />
+      </form> */}
+      <BsThreeDotsVertical size={20} onClick={newUpdate} cursor="pointer"/>
+    </div>
   )
 }
 
