dcba978dc58ce59f558243f50020ae3bdcd718be
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-14 23:28:42 +0500

    add new icons

--- frontend/src/components/sidebar/search/SearchInput.tsx
+++ frontend/src/components/sidebar/search/SearchInput.tsx
@@ -4,7 +4,7 @@ import { IoSearchSharp } from "react-icons/io5";
 const SearchInput = () => {
   return (
 
-    <form className='flex items-center gap-2 border-b-2 border-red-500 text-white'>
+    <form className='flex items-center gap-2 border-b border-white text-white'>
       <IoSearchSharp />
       <input type="text" placeholder='Search...' className='bg-transparent border-none outline-none placeholder:text-white' />
     </form>
