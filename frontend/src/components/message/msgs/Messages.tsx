38afc75cef93cc1f34465e0f420e3ec4ff273765
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-14 23:14:15 +0500

    add loading effect while loading chats

--- frontend/src/components/message/msgs/Messages.tsx
+++ frontend/src/components/message/msgs/Messages.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import useGetMessages from '../../../hooks/useGetMessages'
 import MessageSkeleton from '../../skeletons/MessageSkeleton'
 import Message from './message/Message'
 
 const Messages = () => {
   const { loading, messages } = useGetMessages()
-  // const [loading, setLaoding] = useState(true)
 
   let msg = messages.messages;
   return (
