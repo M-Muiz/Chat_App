815d5edb28a04980771b93eea8dc8a1a5403e354
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-14 23:44:07 +0500

    add dynanmic show ing search box

--- frontend/src/components/sidebar/search/SearchInput.tsx
+++ frontend/src/components/sidebar/search/SearchInput.tsx
@@ -1,20 +1,26 @@
 import { CgSearch } from "react-icons/cg";
 
 import { BsThreeDotsVertical } from "react-icons/bs";
+import toast from "react-hot-toast";
+import { useState } from "react";
 
 
 const SearchInput = () => {
 
-cost
+  const [openSearch, setOpenSearch] = useState(false);
+  console.log(openSearch)
+  const newUpdate = () => {
+    toast("Coming Soon")
+  }
 
   return (
     <div className="flex items-baseline justify-between w-full">
 
-      <CgSearch size={20} cursor="pointer"/>
-      {/* <form className='flex items-center gap-2 border-b border-white text-white'>
+      <CgSearch size={20} cursor="pointer" onClick={() => setOpenSearch(!openSearch)} />
+      {openSearch && <form className='flex items-center gap-2 border-b border-white text-white'>
         <input type="text" placeholder='Search...' className='bg-transparent border-none outline-none placeholder:text-white' />
-      </form> */}
-      <BsThreeDotsVertical size={20} onClick={newUpdate} cursor="pointer"/>
+      </form>}
+      <BsThreeDotsVertical size={20} onClick={newUpdate} cursor="pointer" />
     </div>
   )
 }
