2cdeac796572078bc15835371b353956652b5b16
m-muiz <mmuizsiddiqui@gmail.com>
2024-05-16 00:03:34 +0500

    update config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:9000",
+        target: "https://chat-app-backend-liart-nine.vercel.app",
       },
     },
   },
